a222e1c6fe2ac850e1e6b71c6990ff742476e454
CalebAHJ <calebharperjohnson@gmail.com>
2022-01-10 15:47:42 -0600

    Created hit/blow checker

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import random
 
-def numbergenerator():
+def number_generator():
     digits = list(range(10))
     
     d1 = random.randint(0,9)
@@ -12,5 +12,25 @@ def numbergenerator():
     return str(d1) + str(d2) + str(d3)
 
 
+def hb_checker(guess, answer):
+    hit = 0
+    blow = 0
+
+    for x in range(len(answer)):
+        if guess[x] == answer[x]:
+            hit += 1
+        elif guess[x] == answer[x-1]:
+            blow += 1
+        elif guess[x] == answer[x-2]:
+            blow += 1
+
+    return hit, blow
+
+
 if __name__ == "__main__":
-    print(numbergenerator())
+    num = number_generator()
+    print(num)
+    h, b = hb_checker('123', num)
+    print("Hit: " + str(h))
+    print("Blow: " + str(b))
+    
